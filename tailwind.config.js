abed8992d0c8e344d1841013fa0dcae80bae1feb
siyugg <siyugao52@gmail.com>
2024-02-11 03:59:36 +0800

    Update website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html", "./dist/script.js"],
+  content: ["./index.html", "./script.js"],
   theme: {
     extend: {
       colors: {
